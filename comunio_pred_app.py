53e6470b36691bff2fb0238ce0965741ffe7e7a9
Gobuub <botica1982@msn.com>
2022-09-06 23:13:42 +0200

    'Update images on app'

--- comunio_pred_app.py
+++ comunio_pred_app.py
@@ -39,6 +39,7 @@ def filedownload(df):
     href = f'<a href="data:file/csv;base64,{b64}" download="j_3_data.csv">Download J {journey} Data CSV File</a>'
     return href
 
+
 def squaddownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
@@ -72,16 +73,15 @@ df = input_df
 
 # Displays the user input features
 st.subheader('User Input features')
-#print(df[['Team', 'Player', 'Position', 'Points_Average', 'Value']].columns)
+
 
 if uploaded_file is not None:
     st.write(df[['Team', 'Player', 'Position', 'Points_Average', 'Value']])
 else:
     st.write('Awaiting CSV file to be uploaded. Currently using example input parameters (shown below).')
-    #st.write(df[['Team', 'Player', 'Position', 'Points_Average', 'Value']])
 
 
-# Call function to make predictions
+# Calls function to make predictions
 prediction = ComunioLib.predict_rnn2(df, journey)
 
 # Create an empty df to show the results to the customer
@@ -108,11 +108,11 @@ if uploaded_file is not None:
     else:
         st.write('Input a right Lineup, example : 3-4-3, 4-4-2, ...')
 else:
-    #print(df['squad_img'][0])
+
     img_squad = df.loc[df.Team == teams]['squad_img'].values[0]
-    #print(img_squad)
+
     img_player = df.loc[df.Player == players]['img'].values[0]
-    #print(img_player)
+
     urllib.request.urlretrieve(
         img_squad,
         "image_squad.png")
@@ -124,16 +124,16 @@ else:
 
     st.sidebar.image(image_squad)
     st.subheader('Prediction for next match')
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3 = st.columns(3)  # Make 3 columns to show the info
     with col1:
         st.info(df['Player'].values[0])
         st.image(image_player)
+
     with col2:
         st.info('Position')
         st.subheader(df['Position'].values[0])
         st.info('Next Macht Prediction')
         st.subheader(pred['Prediction'].values[0])
-
 
     with col3:
         st.info('Value')

6ba886775491de8d648ec53c0106835e1e96b2a4
Gobuub <botica1982@msn.com>
2023-05-18 18:37:35 +0200

    Update journey 34

--- comunio_data_train.py
+++ comunio_data_train.py
@@ -15,7 +15,7 @@ from sklearn.metrics import mean_squared_error as mse
 
 df = pd.read_csv('src/data/train/data_train.csv')
 # last update 25/01/2023
-journey = 33
+journey = 34
 # TO ADD NEW JOURNEY
 
 '''
